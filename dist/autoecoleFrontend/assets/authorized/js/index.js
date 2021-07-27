f26065c897483fcc7e528ce2e87fb77dd8798230
Cédric Nguendap <cednguendap@gmail.com>
2021-07-27 05:44:30 +0200

    update dist

---
+++ dist/autoecoleFrontend/assets/authorized/js/index.js
@@ -0,0 +1,23 @@
+import "./modules/sidebar";
+
+import "../scss/app.scss";
+
+// Global
+import "./modules/bootstrap";
+import "./modules/theme";
+import "./modules/feather";
+import "./modules/moment";
+import "./modules/simplebar";
+
+
+
+
+// Forms
+import "./modules/flatpickr";
+
+// Charts
+import "./modules/chartjs";
+
+// Maps
+import "./modules/vector-maps";
+
